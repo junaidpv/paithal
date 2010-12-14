7355600d0c4c8164e92292f5a9fe54d97147d35e
Junaid P V <junu.pv@gmail.com>
2010-12-14 17:29:25 +0400

    using CodeIgniter

--- sites/default/config/config.php
+++ sites/default/config/config.php
@@ -2,20 +2,3 @@
 
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
-
-/**
- * Site's configurations settings
- */
-
-$site = array(
-    'default' =>
-    array(
-        'databse'   => 'paithaldb',
-        'username'  => 'root',
-        'password'  => '123456',
-        'host'  => 'localhost',
-        'port'  => '',
-        'driver'    => 'mysql',
-        'prefix'    => 'pai_',
-    ),
-);
